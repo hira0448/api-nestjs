4ff06edfde94e7881d15727f689d08da7cb8eafa
hirano <rhi691508@gmail.com>
2022-08-23 12:10:33 +0900

    Revert "Install required packages for main.ts and configure interaction with frontend#6"
    This reverts commit 8e051aba83458b95a9e1aacad80dc9465c331861.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { Request } from 'express';
-import * as cookieParper from 'cookie-parser';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors({
-    credentials: true,
-    origin: ['http://localhost: 3000'],
-  });
-  app.use(cookieParper());
   await app.listen(3005);
 }
 bootstrap();
